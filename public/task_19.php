8fa567c7418c5ba731435dca045b123b7d079c2f
mrSemyon <syoma4k@gmail.com>
2022-11-18 14:14:43 +0500

    Finished task_19

--- public/task_19.php
+++ public/task_19.php
@@ -1,100 +1,75 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-        <meta charset="utf-8">
-        <title>
-            Подготовительные задания к курсу
-        </title>
-        <meta name="description" content="Chartist.html">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui">
-        <link id="vendorsbundle" rel="stylesheet" media="screen, print" href="css/vendors.bundle.css">
-        <link id="appbundle" rel="stylesheet" media="screen, print" href="css/app.bundle.css">
-        <link id="myskin" rel="stylesheet" media="screen, print" href="css/skins/skin-master.css">
-        <link rel="stylesheet" media="screen, print" href="css/statistics/chartist/chartist.css">
-        <link rel="stylesheet" media="screen, print" href="css/miscellaneous/lightgallery/lightgallery.bundle.css">
-        <link rel="stylesheet" media="screen, print" href="css/fa-solid.css">
-        <link rel="stylesheet" media="screen, print" href="css/fa-brands.css">
-        <link rel="stylesheet" media="screen, print" href="css/fa-regular.css">
-    </head>
-    <body class="mod-bg-1 mod-nav-link ">
-        <main id="js-page-content" role="main" class="page-content">
-            <div class="row">
-                <div class="col-md-6">
-                    <div id="panel-1" class="panel">
-                        <div class="panel-hdr">
-                            <h2>
-                                Задание
-                            </h2>
-                            <div class="panel-toolbar">
-                                <button class="btn btn-panel waves-effect waves-themed" data-action="panel-collapse" data-toggle="tooltip" data-offset="0,10" data-original-title="Collapse"></button>
-                                <button class="btn btn-panel waves-effect waves-themed" data-action="panel-fullscreen" data-toggle="tooltip" data-offset="0,10" data-original-title="Fullscreen"></button>
-                            </div>
-                        </div>
-                        <div class="panel-container show">
-                            <div class="panel-content">
-                                <div class="panel-content">
-                                    <div class="form-group">
-                                        <form action="">
-                                            <div class="form-group">
-                                                <label class="form-label" for="simpleinput">Image</label>
-                                            <input type="file" id="simpleinput" class="form-control">
-                                            </div>
-                                            <button class="btn btn-success mt-3">Submit</button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+<?php
+$pdo = new PDO("mysql:host=127.0.0.1;dbname=basicsphp;charset=utf8", "root", "root");
 
-                <div class="col-md-6">
-                    <div id="panel-1" class="panel">
-                        <div class="panel-hdr">
-                            <h2>
-                                Загруженные картинки
-                            </h2>
-                            <div class="panel-toolbar">
-                                <button class="btn btn-panel waves-effect waves-themed" data-action="panel-collapse" data-toggle="tooltip" data-offset="0,10" data-original-title="Collapse"></button>
-                                <button class="btn btn-panel waves-effect waves-themed" data-action="panel-fullscreen" data-toggle="tooltip" data-offset="0,10" data-original-title="Fullscreen"></button>
-                            </div>
-                        </div>
-                        <div class="panel-container show">
-                            <div class="panel-content">
-                                <div class="panel-content image-gallery">
-                                    <div class="row">
-                                        <div class="col-md-3 image">
-                                            <img src="img/demo/gallery/1.jpg">
-                                            <a class="btn btn-danger" href="#" onclick="confirm('Вы уверены?');">Удалить</a>
-                                        </div>
-                                        <div class="col-md-3">
-                                            <img src="img/demo/gallery/2.jpg">
-                                            <a class="btn btn-danger" onclick="confirm('Вы уверены?');" href="#">Удалить</a>
-                                        </div>
-                                        <div class="col-md-3">
-                                            <img src="img/demo/gallery/3.jpg">
-                                            <a class="btn btn-danger" onclick="confirm('Вы уверены?');" href="#">Удалить</a>
-                                        </div>
-                                    </div>
+$statement = $pdo->query('SELECT * FROM `images`');
+$images = $statement->fetchAll();
+
+require $_SERVER['DOCUMENT_ROOT'] . '/public/templates/header.php';
+?>
+<div class="row">
+    <div class="col-md-6">
+        <div id="panel-1" class="panel">
+            <div class="panel-hdr">
+                <h2>
+                    Задание
+                </h2>
+                <div class="panel-toolbar">
+                    <button class="btn btn-panel waves-effect waves-themed" data-action="panel-collapse" data-toggle="tooltip" data-offset="0,10" data-original-title="Collapse"></button>
+                    <button class="btn btn-panel waves-effect waves-themed" data-action="panel-fullscreen" data-toggle="tooltip" data-offset="0,10" data-original-title="Fullscreen"></button>
+                </div>
+            </div>
+            <div class="panel-container show">
+                <div class="panel-content">
+                    <div class="panel-content">
+                        <div class="form-group">
+                            <form action="/controllers/task_19.php" enctype="multipart/form-data" method="POST">
+                                <div class="form-group">
+                                    <label class="form-label" for="simpleinput">Image</label>
+                                    <input name="image" type="file" id="simpleinput" class="form-control">
                                 </div>
-                            </div>
+                                <button class="btn btn-success mt-3">Submit</button>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
-
-
-        </main>
-        
-
-        <script src="js/vendors.bundle.js"></script>
-        <script src="js/app.bundle.js"></script>
-        <script>
-            // default list filter
-            initApp.listFilter($('#js_default_list'), $('#js_default_list_filter'));
-            // custom response message
-            initApp.listFilter($('#js-list-msg'), $('#js-list-msg-filter'));
-        </script>
-    </body>
-</html>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div id="panel-1" class="panel">
+            <div class="panel-hdr">
+                <h2>
+                    Загруженные картинки
+                </h2>
+                <div class="panel-toolbar">
+                    <button class="btn btn-panel waves-effect waves-themed" data-action="panel-collapse" data-toggle="tooltip" data-offset="0,10" data-original-title="Collapse"></button>
+                    <button class="btn btn-panel waves-effect waves-themed" data-action="panel-fullscreen" data-toggle="tooltip" data-offset="0,10" data-original-title="Fullscreen"></button>
+                </div>
+            </div>
+            <div class="panel-container show">
+                <div class="panel-content">
+                    <div class="panel-content image-gallery">
+                        <div class="row">
+                            <?php if (!empty($images)) : ?>
+                                <?php foreach ($images as $image) : ?>
+                                    <div class="col-md-3 image">
+                                        <img style="width : 100px;" src="/upload/<?= $image['image'] ?>">
+                                        <a class="btn btn-danger" onclick="if (! confirm('Вы уверены?')) return false;" href="/controllers/task_19.php?del=<?= $image['image'] ?>">
+                                            Удалить
+                                        </a>
+                                    </div>
+                                <?php endforeach ?>
+                            <?php else : ?>
+                                <div class="alert alert-danger fade show">
+                                    Нет доступных для загрузки картинок
+                                </div>
+                            <?php endif ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+require $_SERVER['DOCUMENT_ROOT'] . '/public/templates/footer.php';
